409f494ddc4b172a95ba49b765d2df0e66c541a9
francescotrudu <francescotrudu84@gmail.com>
2023-02-10 17:55:13 +0100

    Import db

--- app/Http/Controllers/Guest/TrainController.php
+++ app/Http/Controllers/Guest/TrainController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class TrainController extends Controller
 {
     //
+    public function index()
+    {
+        //trains è la cartella in view, home è il file da aprire
+        $trains = Train::all();
+        return view('trains.train', compact('trains'));
+    }
 }
